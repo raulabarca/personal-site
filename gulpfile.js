049358ae67957d6709d5611f1d8092a9c61b2ceb
Raul Abarca <raulabarka@gmail.com>
2016-03-02 19:43:32 +0100

    Added jade to html task

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var gulp = require('gulp'),
 // Developer server
 gulp.task('server', function() {
   connect.server({
-    root: './app',
+    root: './app/',
     hostname: '0.0.0.0',
     port: 8080,
     livereload: true,
@@ -18,4 +18,14 @@ gulp.task('server', function() {
     }
   });
 });
-gulp.task('default',	['server']);
+
+// Jade to Html
+gulp.task('jade', function() {
+  return gulp.src('./app/*.jade')
+  .pipe(jade({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./app/'))
+});
+
+// Default task
