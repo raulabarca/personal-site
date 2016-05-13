e5b714a2be2ac64393211ff05a83b9ae7fc3e344
Raul Abarca <raulabarka@gmail.com>
2016-05-13 11:36:11 +0200

    Created fadeIn option

--- pro/js/main.js
+++ pro/js/main.js
@@ -19,8 +19,52 @@ var linkblank = function(){
   });
 };
 
+var animation = function(){
+    $(window).scroll(function(){
+        $('.hideleft').each(function(){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({left: "30",'opacity':'1'},1000);
+
+            }
+        });
+        $('.hideright').each(function(){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({top: "40",'opacity':'1'},1000);
+            }
+        });
+    });
+}
+
+$(document).ready(function() {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({left: "50",'opacity':'1'},1000);
+
+            }
+
+        });
+
+    });
+
+});
 
 
 $(document).ready(linkblank);
 $(document).ready(downscroll);
 $(document).ready(fadesocial);
+$(document).ready(animation);
